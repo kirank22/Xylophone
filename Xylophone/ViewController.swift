42bdea767b0424fdf72f1a0c5b5013f696184ac7
kirank22 <45580946+kirank22@users.noreply.github.com>
2023-12-01 00:09:50 -0500

    dim on touch

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -18,25 +18,23 @@ class ViewController: UIViewController {
     }
 
     @IBAction func keyPressed(_ sender: UIButton) {
-        playSound()
+        playSound(soundName: sender.currentTitle!)
+        
+        //Reduces the sender's (the button that got pressed) opacity to half.
+        sender.alpha = 0.5
+        
+        //Code should execute after 0.2 second delay.
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            //Bring's sender's opacity back up to fully opaque.
+            sender.alpha = 1.0
+        }
     }
     
-    func playSound() {
-        guard let url = Bundle.main.url(forResource: "C", withExtension: "wav") else
-            { return }
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
 
-        do {
-            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
-            try AVAudioSession.sharedInstance().setActive(true)
-
-            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.wav.rawValue)
-
-            guard let player = player else { return }
-            player.play()
-
-        } catch let error {
-            print(error.localizedDescription)
-        }
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player!.play()
     }
 
 }
